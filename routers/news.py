949d8433410f13a3894d5cd810bf23ad2d29b56c
wgz <2961883750@qq.com>
2026-09-01 20:38:22 +0800

    feat: 封装查询数据的方法

--- routers/news.py
+++ routers/news.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from config.db_conf import get_db
 
 # 创建 APIrouter 实例
 # prefix 参数用于设置路由的前缀，tags 参数用于设置路由的标签
@@ -6,9 +9,10 @@ router = APIRouter(prefix="/api/news", tags=["news"])
 
 
 @router.get("/categories")
-async def get_categories(skip: int = 0, limit: int = 100):
+async def get_categories(skip: int = 0, limit: int = 100, db: AsyncSession = Depends(get_db)):
+    categories = await get_categories(db, skip=skip, limit=limit)
     return {
         "code": 200,
         "message": "success",
-        "data": {"新闻分类列表"}
+        "data": categories
     }
