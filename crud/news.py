e324ee386c53fe9b6d32c2e2410221eeb0877170
wgz <2961883750@qq.com>
2026-09-02 17:43:26 +0800

    feat: 添加更新新闻浏览量的数据库操作方法

--- crud/news.py
+++ crud/news.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select, func
+from sqlalchemy import select, func, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.news import Category, News
@@ -31,3 +31,13 @@ async def get_news_detail(db: AsyncSession, news_id: int):
     stmt = select(News).where(News.id == news_id)
     result = await db.execute(stmt)
     return result.scalar_one_or_none()
+
+
+# 增加新闻浏览量
+async def increase_news_views(db: AsyncSession, news_id: int):
+    stmt = update(News).where(News.id == news_id).values(views=News.views + 1)
+    result = await db.execute(stmt)
+    await db.commit()
+
+    # 更新 -> 检查数据库是否真的命中了数据 -> 命中返回True
+    return result.rowcount > 0
