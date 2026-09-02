3a967941c8983700c06231b6297c059df735bf46
wgz <2961883750@qq.com>
2026-09-02 15:28:39 +0800

    feat: 添加获取新闻列表方法

--- crud/news.py
+++ crud/news.py
@@ -1,10 +1,18 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from models.news import Category
+from models.news import Category, News
 
 
 async def get_categories(db: AsyncSession, skip: int = 0, limit: int = 100):
     stmt = select(Category).offset(skip).limit(limit)
+    result = await db.execute(stmt)
+    return result.scalars().all()
+
+
+# 获取新闻列表
+async def get_news_list(db: AsyncSession, category_id: int, skip: int = 0, limit: int = 10):
+    # 查询的是指定分类下的所有新闻
+    stmt = select(News).where(News.category_id == category_id).offset(skip).limit(limit)
     result = await db.execute(stmt)
     return result.scalars().all()
