ccd5ff0ef784ac9fc38e1a05863820659e489639
wgz <2961883750@qq.com>
2026-09-03 19:42:47 +0800

    feat: 实现用户 Token 生成与持久化逻辑

--- crud/users.py
+++ crud/users.py
@@ -1,7 +1,10 @@
+import uuid
+from datetime import datetime, timedelta
+
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from models.users import User
+from models.users import User, UserToken
 from schemas.users import UserRequest
 from utils import security
 
@@ -22,3 +25,23 @@ async def create_user(db: AsyncSession, user_data: UserRequest):
     await db.commit()
     await db.refresh(user)  # 从数据库读回最新的 user 数据
     return user
+
+
+# 生成token
+async def create_token(db: AsyncSession, user_id: int):
+    # 生成 Token + 设置过期时间 -> 查询数据库当前用户是否有 Token -> 有：更新；没有：添加
+    token = str(uuid.uuid4())
+    expires_at = datetime.now() + timedelta(days=7)
+    query = select(UserToken).where(UserToken.user_id == user_id)
+    result = await db.execute(query)
+    user_token = result.scalar_one_or_none()
+
+    if user_token:
+        user_token.token = token
+        user_token.expires_at = expires_at
+    else:
+        user_token = UserToken(user_id=user_id, token=token, expires_at=expires_at)
+        db.add(user_token)
+        await db.commit()
+
+    return token
