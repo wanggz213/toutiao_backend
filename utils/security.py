c65cee868fbd5aa56832d0364f77e4a38172866d
wgz <2961883750@qq.com>
2026-09-07 18:21:55 +0800

    feat(auth): 新增密码验证函数

--- utils/security.py
+++ utils/security.py
@@ -7,3 +7,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # 密码加密
 def get_password_hash(password: str):
     return pwd_context.hash(password)
+
+
+# 验证密码
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
