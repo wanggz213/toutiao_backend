24db7debeaaebbdb2eb69f0ec5c44c623cae686f
wgz <2961883750@qq.com>
2026-09-07 20:18:24 +0800

    feat(user): 新增 UserUpdateRequest 模型

--- schemas/users.py
+++ schemas/users.py
@@ -39,3 +39,12 @@ class UserAuthResponse(BaseModel):
         populate_by_name=True,  # alias / 字段名兼容
         from_attributes=True  # 允许从 ORM 对象属性中取值
     )
+
+
+# 更新用户信息的模型类
+class UserUpdateRequest(BaseModel):
+    nickname: str = None
+    avatar: str = None
+    gender: str = None
+    bio: str = None
+    phone: str = None
