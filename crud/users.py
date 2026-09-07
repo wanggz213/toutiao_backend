07ed1f06734257f2734492f31ab3b6ea30cd5ee1
wgz <2961883750@qq.com>
2026-09-07 20:20:04 +0800

    feat(user): 新增 update_user 局部更新方法

--- crud/users.py
+++ crud/users.py
@@ -1,11 +1,13 @@
 import uuid
 from datetime import datetime, timedelta
 
-from sqlalchemy import select
+from fastapi import HTTPException
+
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.users import User, UserToken
-from schemas.users import UserRequest
+from schemas.users import UserRequest, UserUpdateRequest
 from utils import security
 
 
@@ -70,3 +72,23 @@ async def get_user_by_token(db: AsyncSession, token: str):
     query = select(User).where(User.id == db_token.user_id)
     result = await db.execute(query)
     return result.scalar_one_or_none()
+
+
+# 更新用户信息：update更新 -> 检查是否命中 -> 获取更新后的用户返回
+async def update_user(db: AsyncSession, username: str, user_data: UserUpdateRequest):
+    # update(User).where(User.username == username).values(字段=值，字段=值)
+    # user_data 是一个 Pydantic 模型对象，得到字典 -> ** 解包
+    query = update(User).where(User.username == username).values(**user_data.model_dump(
+        exclude_unset=True,  # 排除未设置的字段
+        exclude_none=True,  # 排除 None 值的字段
+    ))
+    result = await db.execute(query)
+    await db.commit()
+
+    # 检查更新
+    if result.rowcount == 0:
+        return HTTPException(status_code=404, detail="用户不存在")
+
+    # 获取更新后的用户
+    updated_user = await get_user_by_username(db, username)
+    return updated_user
