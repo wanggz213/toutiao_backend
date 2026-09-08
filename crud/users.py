a5ae35b8881f716d993c8ec75c0940a91f27bf69
wgz <2961883750@qq.com>
2026-09-08 13:13:35 +0800

    feat(user): 新增 change_password 修改密码方法

--- crud/users.py
+++ crud/users.py
@@ -92,3 +92,18 @@ async def update_user(db: AsyncSession, username: str, user_data: UserUpdateRequ
     # 获取更新后的用户
     updated_user = await get_user_by_username(db, username)
     return updated_user
+
+
+# 修改密码：验证旧密码 -> 新密码加密 -> 修改密码
+async def change_password(db: AsyncSession, user: User, old_password: str, new_password: str):
+    if not security.verify_password(old_password, user.password):
+        return False
+
+    hashed_new_pwd = security.get_password_hash(new_password)
+    user.password = hashed_new_pwd
+    # 更新：由 sqlalchemy 真正接管这个 User 对象，确保可以 commit
+    # 规避 session 过期或关闭导致的不能提交的问题
+    db.add(user)
+    await db.commit()
+    await db.refresh(user)
+    return True
