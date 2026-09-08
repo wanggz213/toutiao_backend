bdbe6c18dd97272d62cf46c0e1591ed1437fbb48
wgz <2961883750@qq.com>
2026-09-08 17:07:35 +0800

    feat(favorite): 新增取消收藏接口 /remove

--- routers/favorite.py
+++ routers/favorite.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, Query, Depends
+from fastapi import APIRouter, Query, Depends, HTTPException
 
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from starlette import status
 from config.db_conf import get_db
-from crud.favorite import is_news_favorite, add_news_favorite
+from crud import favorite
 from models.users import User
 from schemas.favorite import FavoriteCheckResponse, FavoriteAddRequest
 from utils.auth import get_current_user
@@ -18,7 +18,7 @@ async def check_favorite(
         user: User = Depends(get_current_user),
         db: AsyncSession = Depends(get_db)
 ):
-    is_favorited = await is_news_favorite(db, user.id, news_id)
+    is_favorited = await favorite.is_news_favorite(db, user.id, news_id)
     return success_response(message="检查收藏状态成功", data=FavoriteCheckResponse(isFavorite=is_favorited))
 
 
@@ -28,5 +28,17 @@ async def add_favorite(
         user: User = Depends(get_current_user),
         db: AsyncSession = Depends(get_db)
 ):
-    result = await add_news_favorite(db, user.id, data.news_id)
+    result = await favorite.add_news_favorite(db, user.id, data.news_id)
     return success_response(message="添加收藏成功", data=result)
+
+
+@router.delete("/remove")
+async def remove_favorite(
+        news_id: int = Query(..., alias="newsId"),
+        user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)
+):
+    result = await favorite.remove_news_favorite(db, user.id, news_id)
+    if not result:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="收藏记录不存在")
+    return success_response(message="删除收藏成功")
