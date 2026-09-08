d60c75e0a446ab2c9843af8caf574d3b1221c5a2
wgz <2961883750@qq.com>
2026-09-08 16:29:16 +0800

    feat(favorite): 新增 add_news_favorite 方法

--- crud/favorite.py
+++ crud/favorite.py
@@ -15,3 +15,16 @@ async def is_news_favorite(
     result = await db.execute(query)
     # 检查是否有收藏记录
     return result.scalar_one_or_none() is not None
+
+
+# 添加收藏
+async def add_news_favorite(
+        db: AsyncSession,
+        user_id: int,
+        news_id: int
+):
+    favorite = Favorite(user_id=user_id, news_id=news_id)
+    db.add(favorite)
+    await db.commit()
+    await db.refresh(favorite)
+    return favorite
