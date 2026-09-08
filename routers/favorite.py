f3750153f92af545796535c4c7d596827f4ce145
wgz <2961883750@qq.com>
2026-09-08 16:31:25 +0800

    feat(favorite): 新增添加收藏接口 /add

--- routers/favorite.py
+++ routers/favorite.py
@@ -3,9 +3,9 @@ from fastapi import APIRouter, Query, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from config.db_conf import get_db
-from crud.favorite import is_news_favorite
+from crud.favorite import is_news_favorite, add_news_favorite
 from models.users import User
-from schemas.favorite import FavoriteCheckResponse
+from schemas.favorite import FavoriteCheckResponse, FavoriteAddRequest
 from utils.auth import get_current_user
 from utils.response import success_response
 
@@ -20,3 +20,13 @@ async def check_favorite(
 ):
     is_favorited = await is_news_favorite(db, user.id, news_id)
     return success_response(message="检查收藏状态成功", data=FavoriteCheckResponse(isFavorite=is_favorited))
+
+
+@router.post("/add")
+async def add_favorite(
+        data: FavoriteAddRequest,
+        user: User = Depends(get_current_user),
+        db: AsyncSession = Depends(get_db)
+):
+    result = await add_news_favorite(db, user.id, data.news_id)
+    return success_response(message="添加收藏成功", data=result)
