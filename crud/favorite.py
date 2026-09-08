91d69dab5beca6dcf7470da2d535e8b3a8259580
wgz <2961883750@qq.com>
2026-09-08 17:05:44 +0800

    feat(favorite): 新增 remove_news_favorite 方法

--- crud/favorite.py
+++ crud/favorite.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import select, delete
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -28,3 +28,14 @@ async def add_news_favorite(
     await db.commit()
     await db.refresh(favorite)
     return favorite
+
+
+async def remove_news_favorite(
+        db: AsyncSession,
+        user_id: int,
+        news_id: int
+):
+    stmt = delete(Favorite).where(Favorite.user_id == user_id, Favorite.news_id == news_id)
+    result = await db.execute(stmt)
+    await db.commit()
+    return result.rowcount > 0
