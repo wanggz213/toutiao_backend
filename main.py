a674a90a1c5a0ef931971aeb76a64c55e65dcc56
wgz <2961883750@qq.com>
2026-09-09 16:49:27 +0800

    feat(history): 将浏览功能接口添加到app实例中

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import news, users, favorite
+from routers import news, users, favorite, history
 from fastapi.middleware.cors import CORSMiddleware
 
 from utils.exception_handlers import register_exception_handlers
@@ -30,3 +30,4 @@ async def say_hello(name: str):
 app.include_router(news.router)
 app.include_router(users.router)
 app.include_router(favorite.router)
+app.include_router(history.router)
